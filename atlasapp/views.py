d1506e3361cfb7bc481ec29eb1c67da13c6a0d2b
snirben <snirben@gmail.com>
2021-03-25 00:57:51 +0200

    BSPM2021T18-3-Login Page

--- atlasapp/views.py
+++ atlasapp/views.py
@@ -1,15 +1,29 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate,login
+from django.shortcuts import render, redirect
+from django.contrib import messages
 
 # Create your views here.
 from django.http import HttpResponse
 
 def home (request):
-    return HttpResponse('home page')
+    context = {}
+    return render(request, 'atlasapp/home.html', context)
 
-def register (request):
+def registerpage (request):
     context={}
     return render(request,'atlasapp/register.html',context)
 
-def login (request):
+def loginpage (request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.add_message(
+                request, messages.ERROR, "שם משתמש או סיסמא לא נכונים"
+            )
     context = {}
-    return render(request,'atlasapp/register.html',context)
+    return render(request,'atlasapp/login.html',context)
