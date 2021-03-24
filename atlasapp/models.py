a38be37120b9d1a2575ad2ba3dbb317d442d3a75
snirben <snirben@gmail.com>
2021-03-24 22:07:04 +0200

    BSPM2021T18-2- Create User Table

--- atlasapp/models.py
+++ atlasapp/models.py
@@ -1,3 +1,17 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 # Create your models here.
+
+
+class User(AbstractUser):
+    #define roles
+    ROLE = ((0, "Supervisor"),(1,"Gannet"),(2,"Child"))
+
+    role = models.PositiveSmallIntegerField(choices=ROLE,default=2)
+    mevodad = models.BooleanField(default=False)
+    covid= models.BooleanField(default=False)
+    name = models.CharField(max_length=50,null=False)
+    lastname = models.CharField(max_length=50,null=False)
+    phone = models.CharField(max_length=50,null=False)
+    email = models.CharField(max_length=50,null=False)
--- atlasapp/views.py
+++ atlasapp/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import HttpResponse
+
+def home (request):
+    return HttpResponse('home page')
+
+def register (request):
+    context={}
+    return render(request,'atlasapp/register.html',context)
+
+def login (request):
+    context = {}
+    return render(request,'atlasapp/register.html',context)
