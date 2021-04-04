13e806e15d5206c628f3c2dc805247d00c5efd89
TzlilLevi <57330791+TzlilLevi@users.noreply.github.com>
2021-04-04 12:20:28 +0300

    BSPM2021T18-22-create_mission_and_gan_models

--- atlasapp/models.py
+++ atlasapp/models.py
@@ -1,7 +1,12 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class Gan(models.Model):
+    name = models.CharField(max_length=200, null=False)
+
+    def __str__(self):
+        return '{}'.format(self.name)
 
 
 class User(AbstractUser):
@@ -10,8 +15,17 @@ class User(AbstractUser):
 
     role = models.PositiveSmallIntegerField(choices=ROLE,default=2)
     mevodad = models.BooleanField(default=False)
-    covid= models.BooleanField(default=False)
+    covid = models.BooleanField(default=False)
     name = models.CharField(max_length=50,null=False)
     lastname = models.CharField(max_length=50,null=False)
     phone = models.CharField(max_length=50,null=False)
     email = models.CharField(max_length=50,null=False)
+    gan = models.ForeignKey(Gan,on_delete=models.CASCADE, null=True)
+    
+
+class Mission(models.Model):
+    text = models.TextField(max_length=3000,null=False)
+    done = models.BooleanField(default=False)
+    gannet = models.ForeignKey(Gan, on_delete=models.CASCADE)
+
+
