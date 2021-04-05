34f621dfaf41104f2c9120db5d6425399eefcd45
snirben <snirben@gmail.com>
2021-04-05 22:45:34 +0300

    Hackhaton

--- atlasapp/tests.py
+++ atlasapp/tests.py
@@ -1,3 +1,33 @@
 from django.test import TestCase
 
-# Create your tests here.
+from django.test import TestCase
+from django.test import Client
+from .models import *
+
+
+class UserTestCreate(TestCase):
+
+    def setUp(self):
+        self.gannet = User.objects.get_or_create(
+            username='test_gannet_username',
+            password='test_gannet_password',
+            name='test_gannet_name',
+            lastname='test_gannet_lastname',
+            email='test_gannet_email',
+            phone='test_gannet_phone',
+            mevodad=False,
+            covid=False,
+            role=1)
+        self.child = User.objects.get_or_create(
+            username='test_child_username',
+            password='test_child_password',
+            name='test_child_name',
+            lastname='test_child_lastname',
+            email='test_child_email',
+            phone='test_child_phone',
+            mevodad=False,
+            covid=True,
+            role=2)
+
+
+
