3601068a4cedca8d4f4e37c34c3ca39abbc351ad
TzlilLevi <57330791+TzlilLevi@users.noreply.github.com>
2021-04-08 17:31:31 +0300

    BSPM2021T18-23-add_mission_button

--- atlasapp/urls.py
+++ atlasapp/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('', views.loginpage,name="login"),
     path('register/',views.registerpage, name="register"),
     path('home/',views.home, name="home"),
+    path('SupervisorHome/', views.SupervisorHome, name="SupervisorHome"),
     path('logout/',views.logout_view,name="logout"),
     path('manageMissions/',views.missions_view,name="manageMissions"),
     path(r'^delete/(?P<part_id>[0-9]+)/$', views.delete_mission, name='delete_mission'),
