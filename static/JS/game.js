0f12047cdf1842e940450d5dd6c994c8afa62593
snirben <snirben@gmail.com>
2021-04-27 22:49:24 +0300

    BSPM2021T18-117-JS-cont-steps

--- static/JS/game.js
+++ static/JS/game.js
@@ -1,10 +1,12 @@
 
 $( document ).ready(function() {
 
-var steps=0;
-let firstCard, secondCard;
-
 const cards = document.querySelectorAll('.memory-card');
+var steps = 0;
+var matchcards = 0;
+let hasFlippedCard = false;
+let lockBoard = false;
+let firstCard, secondCard;
 
 function incsteps(){
   steps+=1;
@@ -22,15 +24,70 @@ function flipCard() {
 
 
 
+function checkForMatch() {
+  let isMatch = firstCard.dataset.framework === secondCard.dataset.framework;
+
+  isMatch ? disableCards() : unflipCards();
+}
+
+function disableCards() {
+  firstCard.removeEventListener('click', flipCard);
+  secondCard.removeEventListener('click', flipCard);
+  matchcards+=1;
+  resetBoard();
+
+  if(Checkend()){
+    endgame();
+  }
+
+}
+
+function showsteps(){
+  $("#steps").text(steps);
+}
+function unflipCards() {
+  lockBoard = true;
+
+  setTimeout(() => {
+    firstCard.classList.remove('flip');
+    secondCard.classList.remove('flip');
+
+    resetBoard();
+  }, 1500);
+}
+
 function resetBoard() {
   [hasFlippedCard, lockBoard] = [false, false];
   [firstCard, secondCard] = [null, null];
+}
+
+function Checkend(){
+  if (cards.length/2 === matchcards){
+    return true;
+  }
+  return false;
+}
+function endgame(){
+
+$.ajax({
+            url: "/ajax/save-game-result/",
+            data: {
+                'steps': steps,
+                'game_id': $("section")[0].id
+            },
+            success: function () {
+
+            }
+        });
 }
 
 (function shuffle() {
   cards.forEach(card => {
     let randomPos = Math.floor(Math.random() * 12);
     card.style.order = randomPos;
-  }); });
-});
+  });
+})();
 
+
+cards.forEach(card => card.addEventListener('click', flipCard));
+});
