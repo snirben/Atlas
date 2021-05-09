9fe1702d001d085719558bc35126af88316aaa1c
Linoy <linoyaslan@gmail.com>
2021-05-09 12:54:26 +0300

    Fixing-urls-for-tests

--- atlasapp/urls.py
+++ atlasapp/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
 from . import views
 from django.contrib.auth import views as auth_views
-app_name = "urlss"
 
 urlpatterns = [
     path('', views.loginpage,name="login"),
