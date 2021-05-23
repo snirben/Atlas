ecd4e8658c292f2944f04ad930bcd9f0984801fd
TzlilLevi <57330791+TzlilLevi@users.noreply.github.com>
2021-05-23 18:23:00 +0300

    BSPM2021T18-153_send_mails_to_parents

--- atlas/settings.py
+++ atlas/settings.py
@@ -130,3 +130,11 @@ STATICFILES_DIRS = (
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 MEDIA_URL = "/media/"
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'atlas2021t18@gmail.com'
+EMAIL_HOST_PASSWORD = 'Atlas5109!'
